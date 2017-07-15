773ee3943d22ba6461b6c05d360b813fe157578c
Brandon Hollowell <brandonkhollowell@gmail.com>
2017-07-14 22:34:08 -0400

    Fixed Brandon's stupid mistake.

---
+++ variables.py
@@ -0,0 +1,25 @@
+username = ""
+password = ""
+subreddit = ""
+user_agent = ""
+twitchKey = ""
+client_secret = ""
+client_id = ""
+ddthour = 7
+ftfhour = 7
+ytKey = ""
+schedulerbase = ""
+months = {
+    1:'January ',
+    2:'February ',
+    3:'March ',
+    4:'April ',
+    5:'May ',
+    6:'June ',
+    7:'July ',
+    8:'August ',
+    9:'September ',
+    10:'October ',
+    11:'November ',
+    12:'December '
+}
